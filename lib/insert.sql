4fc93ecdc7a1e658d9d5e85896a98d3e33098397
Njagi Githaiga <njagigithaiga@gmail.com>
2024-06-12 00:25:15 +0300

    Complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, "Mr. Adam", 120, "M", "black", "calm", 0),
+(2, "Ms. Eve", 90, "F", "black", "naive", 0),
+(3, "Sade", 40, "F", "dark brown", "nice", 1),
+(4, "J.Cole", 30, "M", "dark brown", "slimy", 1),
+(5, "Jasmine", 13, "F", "white", "goofy", 1),
+(6, "Richard", 9, "M", "green", "intense", 1),
+(7, "Carol", 2, "F", "yellow", "calm", 1),
+(8, NULL, 25, "M", "black", "aggressive", 0);
